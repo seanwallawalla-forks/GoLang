111fb3f3f6060931846c57fa41f7533673795303
BradMyrick <brad.myrick@protonmail.com>
2021-06-25 10:16:08 -0500

    getting the syntax down and some of the data structures

--- gotest.go
+++ gotest.go
@@ -1,13 +1,34 @@
 package main
 
-import "fmt"
-
-var yes string = "yes"
-var no string = "no"
-var myList []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+//learning GO
+import (
+	"fmt"
+)
 
 func print(str string) {
 	fmt.Println(str)
+}
+
+func MakeSentence(list [7]string) [7]string {
+	var ret [7]string
+	for i := range list {
+		if i == 0 {
+			ret[i] = list[4]
+		} else if i == 1 {
+			ret[i] = list[6]
+		} else if i == 2 {
+			ret[i] = list[5]
+		} else if i == 3 {
+			ret[i] = list[2]
+		} else if i == 4 {
+			ret[i] = list[0]
+		} else if i == 5 {
+			ret[i] = list[1]
+		} else {
+			ret[i] = list[3]
+		}
+	}
+	return ret
 }
 
 func NumInList(list []int, num int) bool {
@@ -21,9 +42,25 @@ func NumInList(list []int, num int) bool {
 }
 
 func main() {
-	if NumInList(myList, 0) {
-		print(yes)
+	yes := "yes"
+	no := "no"
+
+	myList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	strList := [7]string{"of", "bold", "array", "stuff", "Coffee", "an", "is"}
+
+	coin := make(map[int]string)
+	coin[1] = "Bitcoin"
+	coin[2] = "Ethereum"
+	coin[3] = "Doge"
+
+	if NumInList(myList, 5) {
+		no = yes
 	} else {
-		print(no)
+		yes = no
+	}
+
+	sentence := MakeSentence(strList)
+	for _, word := range sentence {
+		print(word)
 	}
 }
